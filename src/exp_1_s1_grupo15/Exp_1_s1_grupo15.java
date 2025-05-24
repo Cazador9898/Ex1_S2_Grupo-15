c9d0c8ba5aa5405a06cecf49a1574cd38105896d
WilaDiaz <wila.diaz@duocuc.cl>
2025-05-24 16:07:59 -0400

    se sube prueba
    Co-Authored-By: Cazador9898 <206640752+Cazador9898@users.noreply.github.com>

--- src/exp_1_s1_grupo15/Exp_1_s1_grupo15.java
+++ src/exp_1_s1_grupo15/Exp_1_s1_grupo15.java
@@ -14,7 +14,7 @@ public class Exp_1_s1_grupo15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola HOLAAAAAA");
     }
     
 }
