947039b3edcb204be473026d60434fbc604937bc
Cazador9898 <alonsoguzmanruz@gmail.com>
2025-05-24 17:10:43 -0400

    Cambiod
    Agregado menu y cdatos de cliente

--- src/exp_1_s1_grupo15/Cliente.java
+++ src/exp_1_s1_grupo15/Cliente.java
@@ -10,4 +10,109 @@ package exp_1_s1_grupo15;
  */
 public class Cliente {
     
+    private String Rut;
+    private String Nombre;
+    private String ApellidoPaterno;
+    private String ApellidoMaterno;
+    private String Domicilio;
+    private String Comuna;
+    private double Telefono;
+    private String CuentaCorriente;
+    private double Saldo;
+
+    public Cliente(String Rut, String Nombre, String ApellidoPaterno, String ApellidoMaterno, String Domicilio, String Comuna, double Telefono, String CuentaCorriente, double Saldo) {
+        this.Rut = Rut;
+        this.Nombre = Nombre;
+        this.ApellidoPaterno = ApellidoPaterno;
+        this.ApellidoMaterno = ApellidoMaterno;
+        this.Domicilio = Domicilio;
+        this.Comuna = Comuna;
+        this.Telefono = Telefono;
+        this.CuentaCorriente = CuentaCorriente;
+        this.Saldo = Saldo;
+    }
+
+    public String getRut() {
+        return Rut;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return ApellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return ApellidoMaterno;
+    }
+
+    public String getDomicilio() {
+        return Domicilio;
+    }
+
+    public String getComuna() {
+        return Comuna;
+    }
+
+    public double getTelefono() {
+        return Telefono;
+    }
+
+    public String getCuentaCorriente() {
+        return CuentaCorriente;
+    }
+
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    public void setRut(String Rut) {
+        this.Rut = Rut;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setApellidoPaterno(String ApellidoPaterno) {
+        this.ApellidoPaterno = ApellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String ApellidoMaterno) {
+        this.ApellidoMaterno = ApellidoMaterno;
+    }
+
+    public void setDomicilio(String Domicilio) {
+        this.Domicilio = Domicilio;
+    }
+
+    public void setComuna(String Comuna) {
+        this.Comuna = Comuna;
+    }
+
+    public void setTelefono(double Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public void setCuentaCorriente(String CuentaCorriente) {
+        this.CuentaCorriente = CuentaCorriente;
+    }
+
+    public void setSaldo(double Saldo) {
+        this.Saldo = Saldo;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+           
+    
+    
 }
--- src/exp_1_s1_grupo15/Exp_1_s1_grupo15.java
+++ src/exp_1_s1_grupo15/Exp_1_s1_grupo15.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package exp_1_s1_grupo15;
+import exp_1_s1_grupo15.Cliente;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 /**
  *
@@ -14,7 +18,82 @@ public class Exp_1_s1_grupo15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola HOLAAAAAA");
+        Scanner sc = new Scanner (System.in);
+        
+        int opcion;
+        
+        
+        do{
+            
+        
+        System.out.println("Bienvenido al Banco BBK");
+        System.out.println("Por Favor seleccione que le Gustaria hacer en su cuenta");
+        System.out.println("1.- Depositar dinero a su cuenta");
+        System.out.println("2.- Girar dinero de su cuenta");
+        System.out.println("3.- Consulta de Saldo");
+        System.out.println("4.- Visualizar sus datos personales");
+        System.out.println("5.- Registrar nuevo usuario a su cuenta");
+        System.out.println("6.- Salir");
+        System.out.println("Por Favor Selecciones una opcion");
+        opcion = sc.nextInt();
+        sc.nextLine();
+            
+        
+            switch (opcion){
+                
+                case 1:
+                    System.out.println("Revisando el estado de la cuenta para el Deposito");
+                    System.out.println("");
+                    
+                    break;
+                    
+                case 2:
+                    System.out.println("Revisando el estado de la cuenta para el Giro");
+                    System.out.println("");
+                    
+                    
+                    break;
+                    
+                case 3:
+                    System.out.println("Revisando estado de cuenta...");
+                    System.out.println("");
+                    
+                    break;
+                    
+                case 4:
+                    System.out.println("Mostrando datos personales de su cuenta");
+                    System.out.println("");
+                    
+                    break;
+                    
+                case 5:
+                    System.out.println("Bienbenido al menu de registro de nuevo Usuario");
+                    System.out.println("");
+                    
+                    break;
+                    
+                case 6:
+                    System.out.println("Gracias por usar nuestros servicios del banco BBK");
+                    System.out.println("Por Favor vuelva pronto");
+                    
+                    break;
+                    
+                default:
+                    System.out.println("Opcion Invalida");
+                    
+  
+                
+            }
+            
+        }while (opcion !=6);
+        
+
+        
+        
+        
+        
+        
+        
     }
     
 }
