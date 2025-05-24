94b975dfa557a40bf3af47b88f4c8a87ab11ac12
WilaDiaz <wila.diaz@duocuc.cl>
2025-05-24 18:12:22 -0400

    se crea clase CuentaCorriente
    Co-Authored-By: Cazador9898 <206640752+Cazador9898@users.noreply.github.com>

---
+++ src/exp_1_s1_grupo15/CuentaCorriente.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exp_1_s1_grupo15;
+
+/**
+ *
+ * @author wdiazc
+ */
+public class CuentaCorriente {
+    private int numeroCuenta;
+    private int saldo;
+    
+    
+    public CuentaCorriente (int numeroCuenta) {
+    this.numeroCuenta = numeroCuenta;
+    this.saldo = saldo;
+   
+    }
+    
+    public int getNumeroCuenta(){
+    return numeroCuenta;
+    }
+    
+    
+    
+    public int getSaldo(){
+    return saldo;    
+    }
+    
+    public void depositar (int monto) {
+    if (monto > 0){
+    saldo += monto;
+    System.out.println("¡Deposito realizado de manuera exitosa!");
+    System.out.println("Saldo Actual: $" + saldo + "pesos.");
+    }else{
+        System.out.println("el monto a ingresar, debe ser mayor a 0.");
+    }   
+}
+    public void girar (int monto){
+        if (monto <= 0){
+    System.out.println("El monto debe ser mayor a cero.");
+        }else if (monto > saldo) {
+    System.out.println("fondos insuficientes.");
+        }else{
+    saldo -= monto;
+    System.out.println("¡Giro realizado de manera exitosa!");
+    System.out.println("Saldo Actual: $" + saldo + "pesos.");
+ }
+}
+    
+    public void consultarSaldo (){
+        System.out.println("Saldo Actual: $" + saldo + "pesos.");
+    
+    }
+}
+
