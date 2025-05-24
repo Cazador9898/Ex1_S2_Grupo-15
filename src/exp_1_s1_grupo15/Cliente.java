6ca13cd5b72b939cfd37d4c62bd75ab878fc8cbe
Wila Diaz <wila.diaz@duocuc.cl>
2025-05-24 15:33:53 -0400

    Add files via upload

---
+++ src/exp_1_s1_grupo15/Cliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exp_1_s1_grupo15;
+
+/**
+ *
+ * @author wdiazc
+ */
+public class Cliente {
+    
+}
---
+++ src/exp_1_s1_grupo15/Exp_1_s1_grupo15.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exp_1_s1_grupo15;
+
+/**
+ *
+ * @author wdiazc
+ */
+public class Exp_1_s1_grupo15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
